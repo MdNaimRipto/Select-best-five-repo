fc6c5506b61d0055303550b9f66675bf68bd81a8
MdNaimRipto <naimurtsc567@gmail.com>
2022-08-22 17:25:38 +0600

    error messages added

--- js/calculate.js
+++ js/calculate.js
@@ -3,6 +3,12 @@ document.getElementById("calculate").addEventListener("click", function () {
     const players = arrayLength();
     const totalPlayer = players.length;
     const perPlayerCost = getInputValueById("per-player-cost");
+
+    if (isNaN(perPlayerCost)) {
+        alert("Not a valid number or box empty. Please check again and provide a valid number.")
+        return;
+    }
+
     const totalPlayerCost = perPlayerCost * totalPlayer;
     setTextValueById("total-player-cost", totalPlayerCost)
 })
@@ -12,6 +18,15 @@ document.getElementById("calculate-total").addEventListener("click", function ()
     const totalPlayerCost = getTextValueById("total-player-cost");
     const managerCost = getInputValueById("manager-cost");
     const coachCost = getInputValueById("coach-cost");
+
+    if (totalPlayerCost == "") {
+        alert("Player's expenceses empty. Please calculate players cost first");
+        return;
+    }
+    if (isNaN(managerCost) || isNaN(coachCost)) {
+        alert("Not a valid number or box empty. Please check again and provide a valid number.")
+        return;
+    }
     const totalExpence = totalPlayerCost + managerCost + coachCost;
     setTextValueById("total-expence", totalExpence);
 })
